75087f01de3545d401f96ab9bf6f1b8ad7f58807
Péter Sturm <sturm.peti@gmail.com>
2020-09-08 14:35:45 +0200

    Added unit tests for GetAllServices
    Related Work Items: #97

---
+++ tests/Lunitor.Configuration.Web.Server.UnitTests/Endpoints/GetAllServicesTests.cs
@@ -0,0 +1,32 @@
+﻿using Lunitor.Configuration.Core;
+using Lunitor.Configuration.Web.Server.Endpoints;
+using Moq;
+using System;
+using Xunit;
+
+namespace Lunitor.Configuration.Web.Server.UnitTests.Endpoints
+{
+    public class GetAllServicesTests
+    {
+        private Mock<IServiceRepository> _serviceRepositoryMock;
+        private Mock<IServiceConfiguratorFactory> _serviceConfiguratorFactoryMock;
+
+        public GetAllServicesTests()
+        {
+            _serviceRepositoryMock = new Mock<IServiceRepository>();
+            _serviceConfiguratorFactoryMock = new Mock<IServiceConfiguratorFactory>();
+        }
+
+        [Fact]
+        public void ConstructorThrowsArgumentNullExceptionIfIServiceRepositoryIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GetAllServices(null, _serviceConfiguratorFactoryMock.Object));
+        }
+
+        [Fact]
+        public void ConstructorThrowsArgumentNullExceptionIfIServiceConfiguratorFactoryIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GetAllServices(_serviceRepositoryMock.Object, null));
+        }
+    }
+}
